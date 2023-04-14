3d4974708edc9abb9f1be5e54ffbd3f296b992c1
Nekuzaky <85588251+Nekuzaky@users.noreply.github.com>
2023-04-14 12:36:33 +0200

    style add

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <footer className="font-bold  text-center bg-gradient-to-r from-fuchsia-500 to-pink-500 text-4xl p-3 font-Karla fixed bottom-0 w-full ">
         <div className=" "> 
-          <p className="text-white text-xl  text-center">  ☆━━━━━━━ By Neku ━━━━━━━━☆ </p>
+          <p className="text-stone-900 text-xl  text-center">  ☆━━━━━━━ By Neku ━━━━━━━━☆ </p>
         </div>
       </footer>
     );
