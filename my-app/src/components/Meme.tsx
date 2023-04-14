64b852d793563da4ba15918f5a8cb0db9396bb60
Nekuzaky <85588251+Nekuzaky@users.noreply.github.com>
2023-04-14 12:15:55 +0200

    UpDate

--- my-app/src/components/Meme.tsx
+++ my-app/src/components/Meme.tsx
@@ -47,6 +47,7 @@ export default function Meme(){
     return (
         <main>
             <div className='flex flex-col sm:justify-around items-center mt-3 justify-center'>
+                <p className="p-2"> ミ★ Reprend le top 100 des memes les plus populaires du moment ★彡 </p>
                 <button className='cursor-pointer bg-gradient-to-r  from-fuchsia-500 via-fuchsia-500 to-pink-500 md:w-[477px] w-96 md:h-[40px] h-16 rounded-md text-white font-Karla' onClick={randomMeme}>↓ Générer un nouveau Meme ↓</button>
                 {/* <div className='relative w-[477px] h-[350px] rounded-sm mt-5 drop-shadow-lg' style={style}>
                     <h2 className="text-3xl font-['Impact'] drop-shadow-lg uppercase tracking-[1px] w-[80%] -translate-x-[50%] text-center absolute text-white left-1/2 top-5">{meme.topText}</h2>
