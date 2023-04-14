ffc4d2d30ec52719fe1b1376d97aafbc6ddee011
Nekuzaky <85588251+Nekuzaky@users.noreply.github.com>
2023-04-14 10:01:10 +0200

    Header + Footer padding

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-      <footer className="font-bold  text-center bg-gradient-to-r from-fuchsia-500 to-pink-500 text-4xl p-4 font-Karla fixed bottom-0 w-full ">
+      <footer className="font-bold  text-center bg-gradient-to-r from-fuchsia-500 to-pink-500 text-4xl p-3 font-Karla fixed bottom-0 w-full ">
         <div className=" "> 
           <p className="text-white text-xl  text-center">  ☆━━━━━━━ By Neku ━━━━━━━━☆ </p>
         </div>
--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
 
   return (
-        <div className=" w-full p-5 gap-5 bg-gradient-to-r  from-fuchsia-500 via-fuchsia-500 to-pink-500">
+        <div className=" w-full p-3 gap-5 bg-gradient-to-r  from-fuchsia-500 via-fuchsia-500 to-pink-500">
           <h1 className="text-4xl font-semibold text-center text-stone-900">
             <p> ⭒☆━━━━━━━ Générateur de meme ━━━━━━━━☆⭒ </p>
           </h1>
