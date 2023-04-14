0ab6ab336e607c08a22395c5513afee710185f5b
Nekuzaky <85588251+Nekuzaky@users.noreply.github.com>
2023-04-14 11:15:42 +0200

    Navbar style add

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
   return (
         <div className=" w-full p-3 gap-5 bg-gradient-to-r  from-fuchsia-500 via-fuchsia-500 to-pink-500">
           <h1 className="text-4xl font-semibold text-center text-stone-900">
-            <p> ⭒☆━━━━━━━ Générateur de meme ━━━━━━━━☆⭒ </p>
+            <p> ⭒☆━━━━━━━ 𝓖é𝓷é𝓻𝓪𝓽𝓮𝓾𝓻 𝓭𝓮 𝓶𝓮𝓶𝓮 ━━━━━━━━☆⭒ </p>
           </h1>
         </div>
   );
