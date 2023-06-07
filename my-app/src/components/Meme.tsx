1e9d713a9068968988bcbc4453ae838c5bdbd5c6
Nekuzaky <85588251+Nekuzaky@users.noreply.github.com>
2023-06-08 00:17:49 +0200

    Update

--- my-app/src/components/Meme.tsx
+++ my-app/src/components/Meme.tsx
@@ -18,7 +18,7 @@ export default function Meme(){
         topText:"",
         bottomText:"",
         imgUrl:'https://i.imgflip.com/64sz4u.png',
-        imgAlt:'No Download??'
+        imgAlt:'No meme ?'
     })
     console.log(allMeme)
     function randomMeme(){
